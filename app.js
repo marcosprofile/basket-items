9f5aa6c004f3f739abef1b541ce4fa73674124bc
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-07-02 05:44:21 -0300

    rename variables

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const quantidadeItem2 = document.getElementById('2');
 const quantidadeItem3 = document.getElementById('3');
 const compraTaxa = document.getElementById('compra-taxa');
 const totalAmount = document.getElementById('total-amount');
+
 const modal = document.getElementById('modal-payment');
 const btnPayment = document.getElementById('btn-payment');
 
@@ -14,21 +15,21 @@ const item1 = 48.00;
 const item2 = 32.00;
 const item3 = 38.00;
 const tax = 28.00;
-let quant1 = 1;
-let quant2 = 1;
-let quant3 = 1;
-let itemsBasket = quant1 + quant2 + quant3;
+let totalItem1 = 1;
+let totalItem2 = 1;
+let totalItem3 = 1;
+let itemsBasket = totalItem1 + totalItem2 + totalItem3;
 
 precoItem1.textContent = `$${item1}`;
 precoItem2.textContent = `$${item2}`;
 precoItem3.textContent = `$${item3}`;
 compraTaxa.textContent = `$${tax}`;
-quantidadeItem1.textContent = quant1;
-quantidadeItem2.textContent = quant2;
-quantidadeItem3.textContent = quant3;
+quantidadeItem1.textContent = totalItem1;
+quantidadeItem2.textContent = totalItem2;
+quantidadeItem3.textContent = totalItem3;
 items.textContent = itemsBasket;
 
-let amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
+let amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
 totalAmount.textContent = `$${amount}`;
 
 /* MODAL */
@@ -46,46 +47,46 @@ function checkout(param) {
 btnPayment.textContent = `Pay $${amount}`;
 
 function adicionar(id) {
-  if (id === 1 && quant1 > 0 && quant1 < 10) {
-    quant1++;
+  if (id === 1 && totalItem1 > 0 && totalItem1 < 10) {
+    totalItem1++;
     itemsBasket++;
-    amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
-  } else if (id === 2 && quant2 > 0 && quant2 < 10) {
-    quant2++;
+    amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
+  } else if (id === 2 && totalItem2 > 0 && totalItem2 < 10) {
+    totalItem2++;
     itemsBasket++;
-    amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
-  } else if (id === 3 && quant3 > 0 && quant3 < 10) {
-    quant3++;
+    amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
+  } else if (id === 3 && totalItem3 > 0 && totalItem3 < 10) {
+    totalItem3++;
     itemsBasket++;
-    amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
+    amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
   }
 
-  quantidadeItem1.textContent = quant1;
-  quantidadeItem2.textContent = quant2;
-  quantidadeItem3.textContent = quant3;
+  quantidadeItem1.textContent = totalItem1;
+  quantidadeItem2.textContent = totalItem2;
+  quantidadeItem3.textContent = totalItem3;
   items.textContent = itemsBasket;
   totalAmount.textContent = `$${amount}`;
   btnPayment.textContent = `Pay $${amount}`;
 }
 
 function remover(id) {
-  if (id === 1 && quant1 > 1) {
-    quant1--;
+  if (id === 1 && totalItem1 > 1) {
+    totalItem1--;
     itemsBasket--;
-    amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
-  } else if (id === 2 && quant2 > 1) {
-    quant2--;
+    amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
+  } else if (id === 2 && totalItem2 > 1) {
+    totalItem2--;
     itemsBasket--;
-    amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
-  } else if (id === 3 && quant3 > 1) {
-    quant3--;
+    amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
+  } else if (id === 3 && totalItem3 > 1) {
+    totalItem3--;
     itemsBasket--;
-    amount = (item1 * quant1) + (item2 * quant2) + (item3 * quant3) + tax;
+    amount = (item1 * totalItem1) + (item2 * totalItem2) + (item3 * totalItem3) + tax;
   }
 
-  quantidadeItem1.textContent = quant1;
-  quantidadeItem2.textContent = quant2;
-  quantidadeItem3.textContent = quant3;
+  quantidadeItem1.textContent = totalItem1;
+  quantidadeItem2.textContent = totalItem2;
+  quantidadeItem3.textContent = totalItem3;
   items.textContent = itemsBasket;
   totalAmount.textContent = `$${amount}`;
   btnPayment.textContent = `Pay $${amount}`;
